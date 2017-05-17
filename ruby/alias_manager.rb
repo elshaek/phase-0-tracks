3be239302ef3793371dcbafc3f52434756c796bc
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-18 01:33:32 +0700

    Update 5.5 code

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,19 +6,19 @@
 # change all the consonants to the next consonant in the alphabet
 # edge case: 'z' will become 'b'
 
+# method to sawp first and last name
 def nameswap(fullname) 
 	fullname.split(" ").reverse.join(" ")
 end
 
-
-
+# method to change all the vowels to the next vowel in 'aeiou'
 def next_vowel(fullname)
 	name_array = fullname.downcase.chars
 	vowels = ["a", "e", "i", "o", "u"]
 
 	name_array.map! do |char|
 		if vowels.include?(char)
-			char_index = vowels.index(char)
+			char_index = vowels.index(char)	# find position of the character in the vowels array
 			char = vowels[char_index + 1]
 		end
 		char
@@ -28,15 +28,14 @@ def next_vowel(fullname)
 	new_name_array.split.map! {|x| x.capitalize}.join(" ") # capitalize each name
 end
 
-
-
+# method to change all the consonants to the next consonant in the alphabet
 def next_consonant(fullname)
 	name_array = fullname.downcase.chars
 	consonants = "bcdfghjklmnpqrstvwxyz"
 
 	name_array.map! do |char|
 		if consonants.include?(char)
-			char_index = consonants.index(char)
+			char_index = consonants.index(char) # find position of the character in the consonants string
 
 			# 'z' will become 'b'
 			if char == "z"
@@ -52,20 +51,28 @@ def next_consonant(fullname)
 	new_name_array.split.map! {|x| x.capitalize}.join(" ") # capitalize each name
 end
 
+
+
 # USER INTERFACE
 # ask for spy's name
 # return an alias
 # keep asking until user types quit
+# print all data before exiting program
 
 user_input = ""
+name_list = {}
+
 until user_input.downcase == "quit"
 	puts "Input name:"
 	user_input = gets.chomp
 	if user_input.downcase == "quit"
+		name_list.each {|user_alias, user_input| puts "#{user_alias} is actually #{user_input}."} # print all data stored in the name_list hash
 		exit
 	elsif user_input.empty?
-		# keep asking for name
+		# keep asking for name if no input
 	else 
-		puts "Your alias: #{nameswap(next_consonant(next_vowel(user_input)))}"
+		user_alias = nameswap(next_consonant(next_vowel(user_input)))
+		name_list[user_alias] = user_input.split.map! {|x| x.capitalize}.join(" ")	# store key-value pair to name_list hash
+		puts "Your alias: #{user_alias}"
 	end
 end
