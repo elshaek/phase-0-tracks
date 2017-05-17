537250d207f21f254ab099728f587f65b1ff78c7
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-18 01:08:21 +0700

    Update 5.5 with user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,18 +1,19 @@
-# RELEASE 0: Attempt a Tricky Algorithm
+# 5.5 Solo Challenge: Manipulating Strings with Iteration
 
 # BUSINESS LOGIC
 # take user input and swap first and last name
 # change all the vowels to the next vowel in 'aeiou'
 # change all the consonants to the next consonant in the alphabet
-# 'z' will become 'b'
+# edge case: 'z' will become 'b'
 
 def nameswap(fullname) 
 	fullname.split(" ").reverse.join(" ")
 end
 
 
+
 def next_vowel(fullname)
-	name_array = fullname.chars
+	name_array = fullname.downcase.chars
 	vowels = ["a", "e", "i", "o", "u"]
 
 	name_array.map! do |char|
@@ -23,30 +24,48 @@ def next_vowel(fullname)
 		char
 	end
 	
-	name_array.join("")
+	new_name_array = name_array.join("")	# put the array back together to get name
+	new_name_array.split.map! {|x| x.capitalize}.join(" ") # capitalize each name
 end
 
+
+
 def next_consonant(fullname)
-	name_array = fullname.chars
+	name_array = fullname.downcase.chars
 	consonants = "bcdfghjklmnpqrstvwxyz"
 
 	name_array.map! do |char|
 		if consonants.include?(char)
 			char_index = consonants.index(char)
-			char = consonants[char_index + 1]
+
+			# 'z' will become 'b'
+			if char == "z"
+				char = "b"
+			else 
+				char = consonants[char_index + 1]
+			end
 		end
 		char
 	end
 	
-	name_array.join("")
+	new_name_array = name_array.join("")	# put the array back together to get name
+	new_name_array.split.map! {|x| x.capitalize}.join(" ") # capitalize each name
 end
-
-puts next_consonant("hello")
 
 # USER INTERFACE
 # ask for spy's name
 # return an alias
+# keep asking until user types quit
 
-#puts "Input name:"
-#user_input = gets.chomp
-#puts "Your alias: #{methodname}"
+user_input = ""
+until user_input.downcase == "quit"
+	puts "Input name:"
+	user_input = gets.chomp
+	if user_input.downcase == "quit"
+		exit
+	elsif user_input.empty?
+		# keep asking for name
+	else 
+		puts "Your alias: #{nameswap(next_consonant(next_vowel(user_input)))}"
+	end
+end
