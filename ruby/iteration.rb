01da038ee20748a02d40b11cb242c61a44fe728f
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-17 13:30:16 +0700

    Update 5.3 iteration.rb code to improve readability

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,115 +1,120 @@
 # RELEASE 0
-# Write your own method that takes a block
-#
-#def method1
-#	puts "Hi, how are you?"
-#	yield
-#end
-#
-#method1 { puts "How's your day?" }
+# Write method that takes a block
+puts "_____This is Release 0_____"
+def method1
+	puts "Hi!"
+	yield("Eric")
+end
+
+method1 { |name1| puts "How's your day, #{name1}?" }
+
+
 
 
 # RELEASE 1
-#
-#food = ["sushi", "pasta", "fruit", "veggie"]
-#p food
-#
-#food.each do |foodtype|
-#	puts "#{foodtype.capitalize} is my favorite food."
-#end
-#
-#modified_food = food.map do |foodtype|
-#	foodtype.capitalize + "!"
-#end
-#
-#puts modified_food
-#
-#
-#
-#animals = {
-#	dog: "beagle",
-#	fish: "salmon",
-#	cat: "lion",
-#	bird: "eagle"
-#}
-#p animals
-#
-#animals.each do |animaltype, animal|
-#	puts "#{animal.capitalize} is a type of #{animaltype}."
-#end
+puts "\n_____Release 1 starts here_____"
+
+food = ["sushi", "pasta", "fruit", "veggie"]
+animals = {
+	dog: "beagle",
+	fish: "salmon",
+	cat: "lion",
+	bird: "eagle"
+}
+
+puts "Using .each and .map! on an array:"
+p food
+
+food.each do |foodtype|
+	puts "#{foodtype.capitalize} is my favorite food."
+end
+
+food.map! do |foodtype|
+	foodtype.capitalize + "!"
+end
+
+puts "After .map call:"
+p food
+
+puts "\nUsing .each on a hash:"
+p animals
+
+animals.each do |animaltype, animal|
+	puts "#{animal.capitalize} is a type of #{animaltype}."
+end
+
+
 
 
 # RELEASE 2
-
-# A method that iterates through the items, deleting any that meet a certain condition (for example, 
-# deleting any numbers that are less than 5).
+puts "\n_____Release 2 starts here_____"
+# A method that iterates through the items, deleting any that meet a certain condition 
+# (for example, deleting any numbers that are less than 5).
+puts "Using .delete_if on array and hash (condition: if number < 3):"
 ary1 = [1, 2, 3, 4]
-
 hash1 = {
-	"1" => "one",
-	"2" => "two",
-	"3" => "three",
-	"4" => "four"
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four"
 }
 
-p ary1
-def numbers_half(ary1)
-	ary1.delete_if {|num| num < 3}
-end
-p numbers_half(ary1)
+puts "Original array: #{ary1}"
+ary1.delete_if {|num| num < 3}
+puts "After running block code: #{ary1}" # the array is changed instantly every time the block is called, not after the iteration is over
 
 
-p hash1
-hash1.delete_if {|digit, num| digit.to_i == 2}
-p hash1
+puts "Original hash: #{hash1}"
+hash1.delete_if {|digit, num| digit < 3}
+puts "After running block code: #{hash1}"
 
 # A method that filters a data structure for only items that do satisfy a certain condition 
 # (for example, keeping any numbers that are less than 5).
+puts "\nUsing .keep_if on array and hash (condition: if number < 3):"
 ary2 = [1, 2, 3, 4]
-
 hash2 = {
-	"1" => "one",
-	"2" => "two",
-	"3" => "three",
-	"4" => "four"
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four"
 }
 
-p ary2
+puts "Original array: #{ary2}"
 ary2.keep_if {|num| num < 3}
-p ary2 #
+puts "After running block code: #{ary2}" 
 
-p hash2
-hash2.keep_if {|digit, num| digit.to_i == 2}
-p hash2 
+puts "Original hash: #{hash2}"
+hash2.keep_if {|digit, num| digit < 3}
+puts "After running block code: #{hash2}"
 
 
 
 # A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
+puts "\nUsing .select on array and  (condition: if number is even):"
 ary3 = [1, 2, 3, 4]
-
 hash3 = {
-	"1" => "one",
-	"2" => "two",
-	"3" => "three",
-	"4" => "four"
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four"
 }
 
-p ary3
+puts "Original array: #{ary3}"
 ary3_1 = ary3.select {|num| num.even?}
-p ary3 			# .select does not change the original array elements
-p ary3_1		# but result can be stored in a new variable
+puts "After running block code: #{ary3}"	# .select does not change the original array elements
+puts "New array: #{ary3_1}"								# but result can be stored in a new variable
 
-p hash3
-hash3_1 = hash3.select {|digit, num| digit.to_i.even?}
-p hash3
-p hash3_1
+puts "Original hash: #{hash3}"
+hash3_1 = hash3.select {|digit, num| digit.even?}
+puts "After running block code: #{hash3}"
+puts "New hash: #{hash3_1}"
 
 
 
-# A method that will remove items from a data structure until the condition in the block evaluates to false, 
-# then stops (you may not find a perfectly working option for the hash, and that's okay).
+# A method that will remove items from a data structure until the condition in the block evaluates to false, then stops
+# (you may not find a perfectly working option for the hash, and that's okay).
+puts "\nUsing .reject on array and hash (condition: if number < 3):"
 ary4 = [1, 2, 3, 4]
-
 hash4 = {
 	1 => "one",
 	2 => "two",
@@ -117,13 +122,13 @@ hash4 = {
 	4 => "four"
 }
 
-p ary4
-ary4_1 = ary4.drop_while {|num| num < 3}
-p ary4
-p ary4_1
+puts "Original array: #{ary4}"
+ary4_1 = ary4.reject {|num| num < 3}
+puts "After running block code: #{ary4}"	# .select does not change the original array elements
+puts "New array: #{ary4_1}"								# but result can be stored in a new variable
 
 
-p hash4
-hash4_1 = hash4.drop_while {|digit, num| digit == 3}
-p hash4
-p hash4_1
+puts "Original hash: #{hash4}"
+hash4_1 = hash4.reject {|digit, num| digit < 3}
+puts "After running block code: #{hash4}"
+puts "New hash: #{hash4_1}"
