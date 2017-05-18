e9e0136b6c5894dca2296fb2540156d4cca97e06
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-18 15:40:51 +0700

    Update 5.6

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,45 +1,52 @@
-
 # RELEASE 0: Implement a Simple search
-
 # create method with 2 parameters: array and integer
+# search array parameter for the integer argument
+# loop through each element in array and compare to integer
+# if integer is included in array, return index of element
+
 def search_array(arr, num)
-  # search array parameter for the integer argument
-  x = 0
+  #x = 0
+  #
+  #while x < arr.length
+  #  if arr[x] == num
+  #    return x
+  #  end
+  #  x += 1
+  #end
 
-  # loop through each element in array and compare to integer
-  while x < arr.length
-    # if integer is included in array, return index of element
+  index = nil
+  arr.each do |x|
     if arr[x] == num
-      return x
+      index = x
+      p index
     end
-    x += 1
   end
-
+  index
 end
 
-
 arr = [42, 89, 23, 1]
-p search_array(arr,24)
+p search_array(arr, 42)
+
+
 
 
 # RELEASE 1 - Calculate Fibonacci Numbers
-
-
 # create method with 1 parameter (x) that is the length of fibonacci array
-def fib(num)
 # create fibonacci array
-fib_arr = []
-
 # iterate through x times
-  num.times do |x|
+# IF index is 0, enter 0 into array
+# IF index is 1, enter 1 into array
+# ELSE add last 2 previous numbers in the array
 
-    # IF index is 0, enter 0 into array
+def fib(num)
+  fib_arr = []
+
+  num.times do |x|
+    
     if x == 0
       fib_arr << 0
-    # IF index is 1, enter 1 into array
     elsif x == 1
       fib_arr << 1
-    # ELSE add last 2 previous numbers in the array
     else
       fib_arr << fib_arr[x-1] + fib_arr[x-2]
     end
@@ -51,30 +58,32 @@ end
 fib(100)
 
 
+
+
 # RELEASE 2 - Sort an array
 # create method that takes an array arg
-def sort_desc(arr)
-  # sort array elements in descending order
+# sort array elements in descending order
+# loop through each element in the array, comparing current number with the next element
+# IF current number is smaller than the next number, then swap
 
+def sort_desc(arr)
   loop do
     swapped = false
 
-    # loop through each element in the array, comparing current number with the next element
     (arr.length-1).times do |x|
-      # IF current number is smaller than the next number, then swap
       if arr[x] < arr[x+1]
           arr[x], arr[x+1] = arr[x+1], arr[x]
           swapped = true
       end
+    end
 
-      end
-
-      if swapped == false
-        break
-      end
+    if swapped == false
+      break
+    end
 
   end
 p arr
 end
+
 arr = [1,5,4,2,10]
 sort_desc(arr)
