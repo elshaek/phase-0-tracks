6cf8ba3ff09a1e99ae01344f57c4f6d632033597
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-23 18:12:27 +0700

    Add puppy characteristics and behavior, and created new puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,40 @@
+# RELEASE 0
+#
+# SPECIES ------------------------
+# Puppy
+#
+# CHARACTERISTICS ----------------
+# Friendly: yes
+# Eye count: 2
+# Paw count: 4
+# Tail length: varies
+# Name: varies
+# Color: varies
+# 
+# BEHAVIOR -----------------------
+# Walk
+# Run
+# Jump
+# Chew
+# Wag tail
+# Bark
+# Growl
+# Whine
+
+# RELEASE 1
+
+class Puppy
+
+end
+
+# p Puppy.methods
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+# p spot.class
+# p duchess == fido
+# p fido.instance_of?(Array)
+# p fido.instance_of?(Puppy)
+# p spot.play_dead
