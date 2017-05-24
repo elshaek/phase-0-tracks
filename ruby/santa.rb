e758df98cca307b1c55114487ade4a76bf4d8d08
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-24 17:39:06 +0700

    Add new Santa class and 3 methods
    3 methods: initialize, speak, eat_milk_and_cookies

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+	def initialize
+		puts "Initializing Santa instance ..."
+	end
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+end
+
+new_santa = Santa.new
+new_santa.speak
+new_santa.eat_milk_and_cookies("snickerdoodle")
