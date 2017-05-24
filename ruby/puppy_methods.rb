bb184e255e8664eb2faed64bc6c204d66e592401
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-24 17:06:07 +0700

    Add lines to separate results from Puppy class and DBC_student class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -21,10 +21,7 @@ class Puppy
   end
 end
 
-
-
-
-
+puts "Create new Puppy instance and call methods on it:"
 
 our_puppy = Puppy.new
 
@@ -33,6 +30,10 @@ our_puppy.speak(5)
 our_puppy.roll_over
 puts our_puppy.dog_years(10)
 our_puppy.bark_at_mailman(80)
+
+puts "\n"
+
+
 
 class DBC_student
 	def initialize
@@ -48,11 +49,16 @@ class DBC_student
 	end
 end
 
+puts "Create 50 new instances of DBC_students and call methods on each of them:"
+
 array_of_DBC_students = Array.new
+
 50.times do |x| 
 	array_of_DBC_students << DBC_student.new
 end
-puts array_of_DBC_students.length
+
+puts "Number of students: #{array_of_DBC_students.length}."
+puts "\n"
 
 array_of_DBC_students.each do |student|
 	student.study(25)
