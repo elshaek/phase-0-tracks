188ecf185b2818f9020c3eeab25e22b3c5141cff
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-24 23:18:09 +0700

    Add 2 singleton methods to Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-	
+	def self.yell_angrily(words)
+    words.upcase + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+  	words + "!" + " :D"
+  end
 end
+
+p Shout.yell_angrily("stop")
+p Shout.yelling_happily("Hi there")
