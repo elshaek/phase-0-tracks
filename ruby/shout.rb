95636438141afe26f701fb67b8344bbd5f2afdcc
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-24 23:43:17 +0700

    Rewrite Shout module as mixin
    Create 2 classes and include Shout singleton methods in them

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,36 @@
-module Shout
-	def self.yell_angrily(words)
-    words.upcase + "!!!" + " :("
-  end
+# module Shout
+# 	def self.yell_angrily(words)
+#     words.upcase + "!!!" + " :("
+#   end
+# 
+#   def self.yelling_happily(words)
+#   	words + "!" + " :D"
+#   end
+# end
+# 
+# p Shout.yell_angrily("stop")
+# p Shout.yelling_happily("Hi there")
 
-  def self.yelling_happily(words)
-  	words + "!" + " :D"
-  end
+module Shout
+	def yell_angrily(words)
+	  words + "!!!" + " :("
+	end
+
+	def yelling_happily(words)
+		words + "!" + " :D"
+	end
 end
 
-p Shout.yell_angrily("stop")
-p Shout.yelling_happily("Hi there")
+class Parents
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
+parent = Parents.new
+puts parent.yelling_happily("great job")
+
+teacher = Teacher.new
+puts teacher.yell_angrily("clean your room")
