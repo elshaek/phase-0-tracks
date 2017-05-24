b3990db4f63796082b0d547efb9fc45d5b574918
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-24 18:24:32 +0700

    Add 4 attributes to Santa class
    4 attribute: gender, ethnicity, reindeer_ranking, age

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,15 +1,28 @@
 class Santa
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
+
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
 end
 
-new_santa = Santa.new
-new_santa.speak
-new_santa.eat_milk_and_cookies("snickerdoodle")
+santa = Santa.new("female", "prefer not to say")
+santa.speak
+santa.eat_milk_and_cookies("snickerdoodle")
+
+santas = []
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
