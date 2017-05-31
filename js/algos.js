f409635c040b70c8703049c9b5e10d50cbea29b3
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-05-31 19:26:19 +0700

    ADD compareObject function that takes two objects and checks to see if the objects share at least one key-value pair

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,4 @@
+// 7.3 Release 0: Find the Longest Phrase
 // Write a function that takes an array of words or phrases and returns the longest word or phrase in the array. 
 
 // use a loop to go through each item in the array
@@ -20,9 +21,38 @@ function longestPhrase(str) {
 	return longestStr;
 }
 
+// 7.3 Release 1: Find a Key-Value Match
+// write a function that takes two objects and checks to see if the objects share at least one key-value pair.
+
+// create a boolean variable (starting with false)
+// use a loop to go through each key-value pair in Object1
+// for each key-value pair in Object1, create a nested loop to compare to each key-value pair in Object2
+// if any key-value pair matches, return true
+// otherwise return false
+
+function compareObject(object1, object2){
+	match = false;
+	for (var name1 in object1) {
+		for (var name2 in object2) {
+			if (name1 == name2 && object1[name1] == object2[name2]) {
+				match = true;
+			}
+		}
+	}
+	return match;
+}
+
+
 // DRIVER CODE
+
+// 7.3 Release 0: Find the Longest Phrase
 var phrases = ["long phrase","longest phrase","longer phrase"];
 console.log(longestPhrase(phrases));
 
-var randomWords = ["hfksogvjf","qwertyuiosdfgh","erghjfghjdkjhgfdfghjk", "asdhgf"];
-console.log(longestPhrase(randomWords));
+var randomLetters = ["hfksogvjf","qwertyuiosdfgh","erghjfghjdkjhgfdfghjk", "asdhgf"];
+console.log(longestPhrase(randomLetters));
+
+// 7.3 Release 1: Find a Key-Value Match
+console.log(compareObject({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(compareObject({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+console.log(compareObject({name: "Steven", age: 54}, {animal: "Dog", legs: 3}));
