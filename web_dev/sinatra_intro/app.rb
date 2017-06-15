ec15636fc0d2468c108fa1c26b5b9b9e236c8868
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-06-15 06:40:20 -0700

    ADD routes for /contact, /great_job that takes a query parameter, route that uses route parameters to add 2 numbers and respond with the result

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,39 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+
+# 9.4 Building a Web Application in Ruby
+
+# RELEASE 0: Add Routes
+# write a GET route that retrieves
+# an address
+
+get '/contact' do
+  "Contact Address:<br>19999 S. Weller Street<br>Seattle, WA 98101"
+end
+
+# write a GET route that
+# takes a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!"
+# IF the query parameter is not present, the route simply says "Good job!"
+
+get '/great_job' do
+  name = params[:name]
+  response = ""
+  if name
+    response = "Good job, #{name}!"
+  else
+    response = "Good job!"
+  end
+  response
+end
+
+# write a GET route that
+# uses route parameters to add two numbers and respond with the result
+
+get '/:num1/:num2' do
+  num1 = params[:num1].to_i
+  num2 = params[:num2].to_i
+  (num1 + num2).to_s
+end
+
