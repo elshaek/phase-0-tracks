8bfecfff591a6aa1589e30b8e265365441a158de
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-06-16 23:01:08 -0700

    ADD multiple event methods, a clickable paragraph

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,3 +1,4 @@
+/*
 function addPinkBorder(event) {
   event.target.style.border = "2px solid pink";
 }
@@ -8,12 +9,38 @@ function removePinkBorder(event) {
 
 var photo = document.getElementById("lizard-photo");
 photo.addEventListener("mouseover", addPinkBorder);
-photo.addEventListener("mouseleave", removePinkBorder);
+photo.addEventListener("mouseleave", removePinkBorder
+*/
+
+$("#lizard-photo").on({
+  mouseenter: function(){
+    $(this).css("border", "2px solid pink");
+  },
+  mouseleave: function(){
+    $(this).css("border", 0);
+  },
+  click: function(){
+    $(this).animate({
+      width: "500px",
+      margin: "80px"
+    })
+    .animate({
+      width: "225px",
+      margin: "0px"
+    });
+  }, 
+});
+
+$("#toggle-buttons").click(function(){
+  $("button").fadeToggle("fast");
+});
+
+
 
 $(".hide").click(function(){
-  $("#lizard-photo").hide();
+  $("#lizard-photo").hide("slow");
 });
 
 $(".reveal").click(function(){
-  $("#lizard-photo").show();
+  $("#lizard-photo").show("fast");
 });
